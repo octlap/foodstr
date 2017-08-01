5dc5fa3aef155837d08a4751e10a66e8a58c0740
Octave Lapeyronie <octave.lapeyronie@gmail.com>
2017-08-01 19:37:05 +0200

    Octave - functioning save new place route

--- routes/private.js
+++ routes/private.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
+const Place = require("../models/place");
 
 router.get("/home", ensureLoggedIn("/login"), (req, res, next) => {
   res.render("private/home");
@@ -10,6 +11,25 @@ router.get("/new", ensureLoggedIn("/login"), (req, res, next) => {
   res.render("private/new");
 });
 
-router.post;
+router.post("/new", ensureLoggedIn("/login"), (req, res, next) => {
+  const newPlace = {
+    name: req.body.name,
+    address: req.body.address,
+    googlePlaceId: req.body.googlePlaceId
+  };
+
+  Place.findOneAndUpdate({ googlePlaceId: req.body.googlePlaceId }, newPlace, {
+    upsert: true,
+    setDefaultsOnInsert: true
+  })
+    .then(res => {
+      console.log("Success!");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  res.redirect("/home");
+});
 
 module.exports = router;
