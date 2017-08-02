250803415ab0d08e1a8beca1e32ea9b28b974d6c
Octave Lapeyronie <octave.lapeyronie@gmail.com>
2017-08-02 13:13:48 +0200

    Octave working add + profile page

--- routes/private.js
+++ routes/private.js
@@ -2,13 +2,18 @@ const express = require("express");
 const router = express.Router();
 const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
 const Place = require("../models/place");
+const User = require("../models/user");
 
+// HOME PAGE //////////////////////////////////////////////////////////////////
 router.get("/home", ensureLoggedIn("/login"), (req, res, next) => {
   res.render("private/home");
 });
 
+// NEW PLACE PAGE /////////////////////////////////////////////////////////////
 router.get("/new", ensureLoggedIn("/login"), (req, res, next) => {
-  res.render("private/new");
+  res.render("private/new", {
+    message: "Tell me about this new place " + req.user.firstName + "!"
+  });
 });
 
 router.post("/new", ensureLoggedIn("/login"), (req, res, next) => {
@@ -18,18 +23,61 @@ router.post("/new", ensureLoggedIn("/login"), (req, res, next) => {
     googlePlaceId: req.body.googlePlaceId
   };
 
+  // First add place in Place collection if it doesn't already exist
   Place.findOneAndUpdate({ googlePlaceId: req.body.googlePlaceId }, newPlace, {
+    new: true,
     upsert: true,
     setDefaultsOnInsert: true
   })
-    .then(res => {
-      console.log("Success!");
+    .then(place => {
+      // Create new place object
+      const userPlace = {
+        _placeId: place._id,
+        status: req.body.beenThere == "on" ? true : false
+      };
+
+      // Only add to user's places vector if not already added
+      return User.findOneAndUpdate(
+        { _id: req.user._id, "places._placeId": { $ne: userPlace._placeId } },
+        { $push: { places: userPlace } },
+        {
+          new: true,
+          setDefaultsOnInsert: true
+        }
+      );
+    })
+    .then(result => {
+      // If place already exists refresh page and suggest adding another one
+      if (result == null) {
+        res.render("private/new", {
+          message:
+            "You've already saved this place " +
+            req.user.firstName +
+            "! Maybe another one?"
+        });
+      } else {
+        res.redirect("/profile");
+      }
     })
     .catch(err => {
       console.log(err);
     });
+});
 
-  res.redirect("/home");
+// PROFILE PAGE ///////////////////////////////////////////////////////////////
+router.get("/profile", ensureLoggedIn("/login"), async (req, res, next) => {
+  const places = [];
+
+  for (let i = 0; i < req.user.places.length; i++) {
+    console.log(req.user.places[i]._placeId);
+    const place = await Place.findOne({ _id: req.user.places[i]._placeId });
+    places.push(place.name);
+  }
+
+  res.render("private/profile", {
+    user: req.user,
+    places
+  });
 });
 
 module.exports = router;
